58d632a5f29c3f9d4da408d6cc9534f6add2694e
organiccat <protoshell@gmail.com>
2015-01-09 14:44:36 -0600

    added add button functionality

--- app/main.js
+++ app/main.js
@@ -2,5 +2,17 @@ app = angular.module('goal-tracker', []);
 
 app.controller('goalController', ['$scope', function($scope) {
   $scope.goals = [];
-  
+  $scope.goalForm = {};
+  $scope.goals.push({
+    name: "Grow Cats",
+    description: "I am growing some cats",
+    difficulty: "Pirate",
+    estimate: "3 days"
+  });
+
+  $scope.addGoal = function() {
+    $scope.goals.push($scope.goalForm);
+    $scope.goalForm = {};
+  };
+
 }]);
