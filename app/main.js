b8ab13443b53c84a65857f4302958d3f0f2b6e1e
organiccat <protoshell@gmail.com>
2015-01-12 16:27:25 -0600

    added files for finish button

--- app/main.js
+++ app/main.js
@@ -7,12 +7,23 @@ app.controller('goalController', ['$scope', function($scope) {
     name: "Grow Cats",
     description: "I am growing some cats",
     difficulty: "Pirate",
-    estimate: "3 days"
+    estimate: "3 days",
+    finished: false
   });
 
   $scope.addGoal = function() {
+    $scope.goalForm.finished = false;
     $scope.goals.push($scope.goalForm);
     $scope.goalForm = {};
+  };
+
+  $scope.removeGoal = function() {
+    var index = $scope.goals.indexOf(this.goal);
+    $scope.goals.splice(index, 1);
+  };
+
+  $scope.finishGoal = function() {
+    this.goal.finished = true;
   };
 
 }]);
